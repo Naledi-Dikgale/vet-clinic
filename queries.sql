000e0e1f87e9409ffa8bde3d4a3372f266c7b7df
naledi-dikgale <naledi22khosi@gmail.com>
2023-06-05 11:57:26 +0200

    alter table

--- queries.sql
+++ queries.sql
@@ -8,3 +8,33 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+--update
+
+UPDATE table animals SET species = 'digimon' WHERE name like '%mon';
+
+UPDATE table animals SET species = 'pokemon' WHERE species = null;
+
+DELETE * FROM animals WHERE date_of_birth > '2022-01-01';
+
+UPDATE * animals SET weight_kg * -1;
+
+UPDATE * animals SET weight_kg * -1 WHERE weight_kg < 0;
+
+--count
+
+SELECT COUNT(*) FROM animals; 
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts =< 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+--WHO escapes the most, neutured or not neutered
+
+SELECT escape_attempts, COUNT(*) FROM animals GROUP BY neutered = true GROUP BY neutered = false;
+
+SELECT MIN(weight_kg) FROM animals WHERE species = 'pokemon' && species = 'digimon';
+
+SELECT MAX(weight_kg) FROM animals WHERE species = 'pokemon' && species = 'digimon';
+
+SELECT AVG(escape_attempts) FROM animals WHERE species = 'pokemon' && species = 'digimon' GROUP BY date_of_birth BETWEEN 'Jan 1, 1990' AND 'Dec 31, 2000';
--- schema.sql
+++ schema.sql
@@ -1,13 +1,11 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE DATABASE vet_clinic;
-USE vet_clinic;
-
 CREATE TABLE animals (
    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(200),
   date_of_birth DATE,
   escape_attempts INT,
   neutered BOOLEAN,
-  weight_kg DECIMAL(5, 2)
+  weight_kg DECIMAL
 );
+
+ALTER TABLE animals ADD species VARCHAR(200);
