66890101a153ddf72d3f4e3d206c0da8f58f13f5
naledi-dikgale <naledi22khosi@gmail.com>
2023-06-06 15:46:06 +0200

    Update schema.sql file

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 CREATE TABLE animals (
-   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+   id SERIAL NOT NULL,
   name VARCHAR(200),
   date_of_birth DATE,
   escape_attempts INT,
@@ -13,46 +13,23 @@ ALTER TABLE animals ADD species VARCHAR(200);
 -- multiple tables
 
 -- Create table owners
-CREATE TABLE owners (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  full_name TEXT,
-  age INTEGER
+CREATE TABLE owners(
+  id SERIAL NOT NULL,
+  full_name VARCHAR(200),
+  age INT,
+  PRIMARY KEY(id)
 );
 
 -- Create table species
-CREATE TABLE species (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT
+CREATE TABLE species(
+  id SERIAL NOT NULL,
+  name VARCHAR(200),
+  PRIMARY KEY(id)
 );
 
--- Modify animals table
-CREATE TABLE animals_backup (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
-  species_id INTEGER,
-  owner_id INTEGER,
-  FOREIGN KEY (species_id) REFERENCES species(id),
-  FOREIGN KEY (owner_id) REFERENCES owners(id)
-);
+-- modify
+ALTER TABLE animals DROP species;
 
--- Copy existing data to backup table
-INSERT INTO animals_backup SELECT * FROM animals;
+ALTER TABLE animals ADD species_id INT REFERENCES species(id);
 
--- Drop old animals table
-DROP TABLE animals;
-
--- new animals table
-CREATE TABLE animals (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
-  species_id INTEGER,
-  owner_id INTEGER,
-  FOREIGN KEY (species_id) REFERENCES species(id),
-  FOREIGN KEY (owner_id) REFERENCES owners(id)
-);
-
--- Copy data back new animals table
-INSERT INTO animals SELECT * FROM animals_backup;
-
--- Drop backup table
-DROP TABLE animals_backup;
+ALTER TABLE animals ADD owner_id INT REFERENCES owners(id);
