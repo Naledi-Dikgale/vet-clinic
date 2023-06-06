598037681bf7a5e7e4e6db4c13139ad75c7e33d4
naledi-dikgale <naledi22khosi@gmail.com>
2023-06-06 09:52:15 +0200

    Update schema.sql file

--- schema.sql
+++ schema.sql
@@ -9,3 +9,50 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species VARCHAR(200);
+
+-- multiple tables
+
+-- Create table owners
+CREATE TABLE owners (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  full_name TEXT,
+  age INTEGER
+);
+
+-- Create table species
+CREATE TABLE species (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+-- Modify animals table
+CREATE TABLE animals_backup (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  species_id INTEGER,
+  owner_id INTEGER,
+  FOREIGN KEY (species_id) REFERENCES species(id),
+  FOREIGN KEY (owner_id) REFERENCES owners(id)
+);
+
+-- Copy existing data to backup table
+INSERT INTO animals_backup SELECT * FROM animals;
+
+-- Drop old animals table
+DROP TABLE animals;
+
+-- new animals table
+CREATE TABLE animals (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  species_id INTEGER,
+  owner_id INTEGER,
+  FOREIGN KEY (species_id) REFERENCES species(id),
+  FOREIGN KEY (owner_id) REFERENCES owners(id)
+);
+
+-- Copy data back new animals table
+INSERT INTO animals SELECT * FROM animals_backup;
+
+-- Drop backup table
+DROP TABLE animals_backup;
